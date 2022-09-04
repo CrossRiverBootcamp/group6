3633acf27696f8adc0a21c27fdf8b2f11e3da374
miriamblau42 <miriblau42@gmail.com>
2022-09-04 16:27:10 +0300

    data-Layer

---
+++ CustomerAccount.Data/AccountDal.cs
@@ -0,0 +1,61 @@
+﻿
+using CustomerAccount.Data.Entities;
+using CustomerAccount.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerAccount.Data
+{
+    public class AccountDal : IAccountDal
+    {
+        private IDbContextFactory<CustomerAccountContext> _contextFactory;
+
+        public AccountDal(IDbContextFactory<CustomerAccountContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+        
+        public bool CreateAccount(Account account, Customer customer)
+        {
+            using var _contect = _contextFactory.CreateDbContext();
+            account.Customer = customer;
+            try
+            {
+                 _contect.Customers.AddAsync(customer);
+                 _contect.Accounts.AddAsync(account);
+                 _contect.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+
+        }
+
+        public async Task<bool> EmailExists(string email)
+        {
+            using var _contect = _contextFactory.CreateDbContext();
+            return await _contect.Customers.AnyAsync(c => c.Email == email);
+        }
+
+        public  async Task<Account> GetAccountInfo(int accountID)
+        {
+            using var _contect = _contextFactory.CreateDbContext();
+            try
+            {
+                //include customer
+                return await _contect.Accounts.FirstAsync(c => c.ID == accountID);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public int Login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomerAccount.Data/Entities/Account.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomerAccount.Data.Entities
+{
+    //internal and allow only bl to be invoved
+    public class Account
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public DateTime OpenDate { get; set; }
+        public double Balance { get; set; }
+        public int CustomerID { get; set; }
+        [ForeignKey("CustomerID")]
+        public virtual Customer Customer { get; set; }
+
+    }
+}
