abba445f90896d9dbb36d903a549fa88c1bfb4f6
miriamblau42 <miriblau42@gmail.com>
2022-09-04 19:57:43 +0300

    controllers ,Dtos

---
+++ CustomerAccount.WebAPI/MapperDtoModels.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CustomerAccount.Services.Models;
+using CustomerAccount.WebAPI.DTOs;
+
+namespace CustomerAccount.WebAPI
+{
+    public class MapperDtoModels :Profile
+    {
+        public MapperDtoModels()
+        {
+            CreateMap<RegisterDTO, RegisterModel>();
+            CreateMap<AccountModel, AccountInfoDTO>();
+        }
+    }
+}
