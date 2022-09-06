2a6b29d966de4603ef98f0382ccae744904f2d47
מרים <miriamkaplan123@gmail.com>
2022-09-06 15:38:41 +0300

    messages.commands

---
+++ server-project/Messages/Commands/UpdateBalance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages.Commands
+{
+    internal class UpdateBalance
+    {
+        public int TransactionID { get; set; }
+        public int FromAccountID { get; set; }
+        public int ToAccountID { get; set; }
+        public int Amount { get; set; }
+    }
+}
