bd40494039f69114104e15d893313d9acae8cf0e
miriamblau42 <miriblau42@gmail.com>
2022-09-15 14:35:10 +0300

    details in dialog

--- CrossRiverBank-client/src/app/modules/customer/foreign-account-info/foreign-account-info.component.ts
+++ CrossRiverBank-client/src/app/modules/customer/foreign-account-info/foreign-account-info.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ForeignAccountDTO } from 'src/app/models/foreignAccountDetailsDTO.models';
 
 @Component({
   selector: 'app-foreign-account-info',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./foreign-account-info.component.css']
 })
 export class ForeignAccountInfoComponent implements OnInit {
-
-  constructor() { }
+    accountDetails! : ForeignAccountDTO;
+  constructor(public _dialog: MatDialog,@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
+    debugger
+    this.accountDetails = this.data.details;
+  }
+  CloseDialog()
+  {
+    this._dialog.closeAll();
   }
 
 }
--- CrossRiverBank-client/src/app/modules/customer/operations-history/operations-history.component.ts
+++ CrossRiverBank-client/src/app/modules/customer/operations-history/operations-history.component.ts
@@ -1,5 +1,6 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator, MatPaginatorIntl, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ForeignAccountDTO } from 'src/app/models/foreignAccountDetailsDTO.models';
@@ -7,16 +8,17 @@ import { getOperationDTO } from 'src/app/models/getOperationDTO.models';
 import { OperationsHistoryDTO } from 'src/app/models/OperationsHistoryDTO.models';
 import { LoginService } from 'src/app/services/login.service';
 import { OperationsService } from 'src/app/services/operation.service';
+import { ForeignAccountInfoComponent } from '../foreign-account-info/foreign-account-info.component';
 
 
 @Component({
   selector: 'app-operations-history',
-  templateUrl: './operations-history1.componant.html',
+  templateUrl: './operations-history.component.html',
   styleUrls: ['./operations-history.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
@@ -28,18 +30,20 @@ export class OperationsHistoryComponent implements OnInit {
   numOfOperaitons: number = 0;
   pageSizeOptions = [2, 4, 6];
   index1 = 0;
-    currentAccountID = 0;
-    pageNumber = 0;
-    numberOfRecords =10;
+  currentAccountID = 0;
+  pageNumber = 0;
+  numberOfRecords = 10;
   // };
   foreignAccountDetails: ForeignAccountDTO | null = {
-    accountId:0,
-    firstName:" ",
-    lastName:" ",
-    email:" ",
+    accountId: 0,
+    firstName: " ",
+    lastName: " ",
+    email: " ",
   };
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  constructor(private _loginService: LoginService, private _operationsService: OperationsService) { }
+  constructor(private _loginService: LoginService, private _operationsService: OperationsService, private _dialog: MatDialog) {
+
+  }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -49,18 +53,7 @@ export class OperationsHistoryComponent implements OnInit {
     this.getNumOfOperations();
     this.getOperationsFromDB();
   }
-  //get foreign account details when expansing!
-  getforeignAccountDetails(accountID: number) {
-    this._loginService.GetForeignAccountDetails(accountID).subscribe((res) => {
-      this.foreignAccountDetails = {
-        accountId:accountID,
-        firstName : res.firstName,
-        lastName : res.lastName,
-        email : res.email,
-      };
-      
-    }, (err) => { console.log(err); });
-  }
+
   getNumOfOperations() {
     this._operationsService.setnumberOperation(this.currentAccountID).subscribe((res) => {
       //intialize total number of operations-history
@@ -68,25 +61,24 @@ export class OperationsHistoryComponent implements OnInit {
     }, (err) => { console.log(err); });
   }
   getOperationsFromDB() {
-      const getOperationDT0 : getOperationDTO ={
-        currentAccountID : this.currentAccountID,
-        pageNumber :this.pageNumber,
-      numberOfRecords:this.numberOfRecords,
-      }
+    const getOperationDT0: getOperationDTO = {
+      currentAccountID: this.currentAccountID,
+      pageNumber: this.pageNumber,
+      numberOfRecords: this.numberOfRecords,
+    }
     this._operationsService.getOperation(getOperationDT0).subscribe(
       (data) => {
-        if(data)
-        {
+        if (data) {
           this.dataSource.data = data;
           this.paginator.pageIndex = this.pageNumber;
           setTimeout(() => {
             this.paginator.pageIndex = this.pageNumber;
-            this.paginator.length =this.numOfOperaitons;
-          }) 
+            this.paginator.length = this.numOfOperaitons;
+          })
         }
-       else{
-        alert("dont have any more operations");
-       }
+        else {
+          alert("dont have any more operations");
+        }
       }, (err) => {
         alert("faild to get your operations");
       }
@@ -97,6 +89,27 @@ export class OperationsHistoryComponent implements OnInit {
     this.pageNumber = pageEvent.pageIndex;
     this.numberOfRecords = pageEvent.pageSize;
     this.getOperationsFromDB();
+  }
+  //get foreign account details when expansing!
+  getforeignAccountDetails(accountID: number) {
+    this._loginService.GetForeignAccountDetails(accountID).subscribe((res) => {
+      this.foreignAccountDetails = {
+        accountId: accountID,
+        firstName: res.firstName,
+        lastName: res.lastName,
+        email: res.email,
+      };
+      this.openDialog(this.foreignAccountDetails);
+
+    }, (err) => { console.log(err); });
+  }
+  openDialog(details: ForeignAccountDTO) {
+    debugger
+    const dialogRef = this._dialog.open(ForeignAccountInfoComponent, {
+      data: {
+        accountDetails: details
+      },
+    });
   }
 
 
