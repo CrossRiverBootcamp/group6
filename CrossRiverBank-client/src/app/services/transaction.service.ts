1737378fe265a445258ec7a4c9772460bafdea1f
מרים <miriamkaplan123@gmail.com>
2022-09-18 20:41:44 +0300

    design

--- CrossRiverBank-client/src/app/services/transaction.service.ts
+++ CrossRiverBank-client/src/app/services/transaction.service.ts
@@ -3,9 +3,6 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { AddTransactionDTO } from "../models/addTransactionDTO.models";
 
-
-
-
 @Injectable({
   providedIn: 'root'
 })
@@ -13,7 +10,6 @@ export class TransactionService {
   private transactionUrl = 'https://localhost:7206/api/Transaction';
 
   addTransaction(addTransaction: AddTransactionDTO): Observable<boolean> {
-    debugger;
     return this._http.post<boolean>(this.transactionUrl, addTransaction);
   }
   constructor(private _http: HttpClient) { }
