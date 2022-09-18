3518e9ce3a6917ddd2dad9e7280592bc312c1e89
מרים <miriamkaplan123@gmail.com>
2022-09-18 21:27:32 +0300

    deleted

--- CustomerAccount.WebAPI/DTOs/AccountInfoDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CustomerAccount.WebAPI.DTOs
-{
-    public class AccountInfoDTO
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public double Balance { get; set; }
-    }
-}
