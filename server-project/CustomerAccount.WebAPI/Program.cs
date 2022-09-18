e5cf28c52f7cf675f1affb7682af169215084507
מרים <miriamkaplan123@gmail.com>
2022-09-18 23:16:49 +0300

    ServiceExtension

--- server-project/CustomerAccount.WebAPI/Program.cs
+++ server-project/CustomerAccount.WebAPI/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using NServiceBus;
 using Microsoft.Data.SqlClient;
+using CustomerAccount.WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -79,40 +80,11 @@ builder.Services.AddAuthentication(x =>
     };
 });
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CrossRiverBank", Version = "v1" });
-
-    // To Enable authorization using Swagger (JWT)    
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                          new OpenApiSecurityScheme
-                            {
-                                Reference = new OpenApiReference
-                                {
-                                    Type = ReferenceType.SecurityScheme,
-                                    Id = "Bearer"
-                                }
-                            },
-                            new string[] {}
-
-                    }
-                });
-});
-
+builder.Services.AddSwaggerSettings();
 
 Log.Logger = new LoggerConfiguration().ReadFrom.Configuration
             (configuration).CreateLogger();
+
 
 
 var app = builder.Build();
